a910392412ab80ba024a22db583b2ab91accd9c4
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-20 20:42:56 +0300

    Warext 2.2 bootstrap sürümünü güncelle

--- upload/js/warext/turkish-spellcheck/bootstrap-v110.js
+++ upload/js/warext/turkish-spellcheck/bootstrap-v110.js
@@ -4,7 +4,7 @@
   if (window.__warextTurkishSpellBootstrapV110) return;
   window.__warextTurkishSpellBootstrapV110 = true;
 
-  const VERSION = '2.1.0';
+  const VERSION = '2.2.0';
   const script = document.currentScript;
   const scriptUrl = script?.src || '';
   const baseDir = scriptUrl ? scriptUrl.slice(0,scriptUrl.lastIndexOf('/') + 1) : '';
